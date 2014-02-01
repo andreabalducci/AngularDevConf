79c873ee8343e5f6f8ae83f208dab88fee27fb54
Andrea Balducci <mtb.snowboard@gmail.com>
2014-02-01 15:22:23 +0100

    added filter on local service

--- app/services/catalogService.js
+++ app/services/catalogService.js
@@ -4,7 +4,19 @@ angular.module('catalog', []).service('catalogService', function($q, $http){
         var defer = $q.defer();
 
         $http.get('../catalog/index.json').then(function(results){
-            defer.resolve(results.data);
+
+            var searchResult = results.data;
+
+            if(term){
+                var expression = new RegExp(term,"i");
+
+                searchResult = _.filter(searchResult, function(item){
+                    return  expression.test(item.title)||
+                            expression.test(item.location);
+                });
+            }
+
+            defer.resolve(searchResult);
         });
 
         return defer.promise;
