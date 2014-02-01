a4987132f71f745361a5fe39287dff20470aef4a
Andrea Balducci <mtb.snowboard@gmail.com>
2014-02-01 16:13:56 +0100

    live search (incomplete)

--- app/layout/layoutCtrl.js
+++ app/layout/layoutCtrl.js
@@ -2,7 +2,6 @@ angular.module('ngDevConf')
     .controller('layoutCtrl', function ($scope, $location) {
 
         $scope.debug = false;
-
         $scope.toggleDebugMode = function () {
             $scope.debug = !$scope.debug;
         }
--- app/layout/navbarCtrl.js
+++ app/layout/navbarCtrl.js
@@ -1,6 +1,15 @@
 angular.module('ngDevConf')
     .controller('navbarCtrl', function ($scope, $location) {
+        var liveSearchEnabled = true;
+
         $scope.doSearch = function (term) {
-            $location.path('search').search({q:term || ''});
+            term = term || $scope.term || '';
+            $location.path('search').search({q:term});
+        }
+
+        $scope.searchTextChanged = function(){
+            if(liveSearchEnabled){
+                $scope.doSearch();
+            }
         }
     });
