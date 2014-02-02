8b91b10956d50852775e33dd1e9e69acc62a47b3
Andrea Balducci <mtb.snowboard@gmail.com>
2014-02-02 10:41:13 +0100

    fixed service catalog improvements

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,18 +1,5 @@
-angular.module('ngDevConf').controller('homeCtrl', function ($scope) {
-    $scope.vm = {
-        organizers: [
-            {
-                "logo": "../img/logoUNIVPM.png",
-                "text": "Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna."
-            },
-            {
-                "logo": "../img/gdi-logo-03.png",
-                "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Crasmattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh."
-            },
-            {
-                "logo": "../img/devmarche.png",
-                "text": "Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
-            }
-        ]
-    }
+angular.module('ngDevConf').controller('homeCtrl', function ($scope, catalogService) {
+    catalogService.getHomeData().then(function(homeData){
+        $scope.vm = homeData;
+    });
 });
--- app/services/catalogService.js
+++ app/services/catalogService.js
@@ -3,6 +3,45 @@ angular.module('catalog', []).service('catalogService', function ($q, $http, $lo
     var localCache;
     var mapById = {};
 
+    var homeData =  {
+        opening:{
+            "logo" :"../img/AngularJS-large.png",
+            "spot" : "Angular is what HTML would have been had it been designed for applications",
+            "title" :"Introduzione ad AngularJS",
+            "text" :"Come realizzare Single Page Applications sfruttando databinding bidirezionale, templates, routing, controllers, componenti e servizi web creando passo passo un catalogo immagini.",
+            "link" : {
+                "text" : "Approfondisci »",
+                "url" : "http://www.dii.univpm.it/angulardevconf"
+            }
+        },
+        marketing: [
+            {
+                "logo": "../img/logoUNIVPM.png",
+                "text": "Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+                "link":{
+                    title:"Vai al portale »",
+                    url:"http://www.dii.univpm.it/"
+                }
+            },
+            {
+                "logo": "../img/gdi-logo-03.png",
+                "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Crasmattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+                "link":{
+                    title:"Vai al blog »",
+                    url:"http://developersitalia.blogspot.it/"
+                }
+            },
+            {
+                "logo": "../img/devmarche.png",
+                "text": "Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
+                "link":{
+                    title:"Vai al blog »",
+                    url:"http://dev.marche.it/"
+                }
+            }
+        ]
+    };
+
     function filter(items, term) {
         if (!term)
             return items;
@@ -15,8 +54,6 @@ angular.module('catalog', []).service('catalogService', function ($q, $http, $lo
             if (expression.test(item.title) || expression.test(item.location)) {
                 matches.push(item);
             }
-
-            mapById[item.id] = item;
         });
 
         return matches;
@@ -28,6 +65,10 @@ angular.module('catalog', []).service('catalogService', function ($q, $http, $lo
         if (!localCache) {
             $http.get('../catalog/index.json').then(function (results) {
                 localCache = results.data;
+                angular.forEach(localCache, function (item) {
+                    mapById[item.id] = item;
+                });
+
                 defer.resolve(localCache);
             });
         } else {
@@ -56,6 +97,12 @@ angular.module('catalog', []).service('catalogService', function ($q, $http, $lo
             defer.resolve(mapById[id]);
         });
 
+        return defer.promise;
+    }
+
+    this.getHomeData = function () {
+        var defer = $q.defer();
+        defer.resolve(homeData);
         return defer.promise;
     }
 });
